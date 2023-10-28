2e9ade1b0ba4497cc8047f72b0b71d186d385a65
41w15 <ostrovskiedvin0@gmail.com>
2023-10-29 00:59:45 +0300

    Updated changelog

--- js/mod.js
+++ js/mod.js
@@ -13,11 +13,13 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.01",
+	name: "Nothing new",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
+	<h3>v0.01</h3><br>
+		- Added name to the game
 	<h3>v0.0</h3><br>
 		- Added things.<br>
 		- Added stuff.`
